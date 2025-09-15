d3a034af47451ebad162bea9aba341509e94d6bb
Placide0007 <lazareplacide02@gmail.com>
2025-09-15 10:24:28 +0200

    update .env

--- config/cors.php
+++ config/cors.php
@@ -6,7 +6,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://lib-front-klhy.vercel.app'],
+    'allowed_origins' => ['https://api-libmanagement-production.up.railway.app'],
 
     'allowed_origins_patterns' => [],
 
